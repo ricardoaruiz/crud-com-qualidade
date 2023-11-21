ccd6381773824d89443616a1f03fafecbbc86f41
Ricardo Ruiz <ricardo.ruiz@grupoboticario.com.br>
2023-11-20 21:20:56 -0300

    remove a todo

--- core/crud.ts
+++ core/crud.ts
@@ -7,6 +7,11 @@ import { v4 as uuid } from 'uuid'
 const DB_FILE_PATH = './core/db';
 
 /**
+ * The UUID type.
+ */
+type UUID = string;
+
+/**
  * An interface representing a todo.
  */
 interface Todo {
@@ -44,12 +49,12 @@ function createTodo(content: string): Todo {
  * Updates a todo item with the given id by merging it with the properties 
  * from the partialTodo object.
  *
- * @param {string} id - The id of the todo item to update.
+ * @param {UUID} id - The id of the todo item to update.
  * @param {Partial<Todo>} partialTodo - The partial todo object containing 
  *     the properties to merge with the existing todo item.
  * @return {Todo} - The updated todo item.
  */
-function updateTodo(id: string, partialTodo: Partial<Todo>): Todo {
+function updateTodo(id: UUID, partialTodo: Partial<Todo>): Todo {
   
   const todos = readTodos();
 
@@ -68,28 +73,51 @@ function updateTodo(id: string, partialTodo: Partial<Todo>): Todo {
 
   fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos}, null, 2));
 
-  return todoToUpdate
+  return todoToUpdate;
 }
 
 /**
  * Updates the content of a todo item based on its ID.
  *
- * @param {string} id - The ID of the todo item.
+ * @param {UUID} id - The ID of the todo item.
  * @param {string} content - The new content for the todo item.
  * @return {Todo} - The updated todo item.
  */
-function updateTodoContentById(id: string, content: string): Todo {
+function updateTodoContentById(id: UUID, content: string): Todo {
   return updateTodo(id, { content });
 }
 
 /**
  * Marks a todo as done.
  *
- * @param {string} id - The ID of the todo to mark as done.
+ * @param {UUID} id - The ID of the todo to mark as done.
  * @return {Todo} The updated todo.
  */
-function markTodoAsDone(id: string): Todo {
+function markTodoAsDone(id: UUID): Todo {
   return updateTodo(id, { done: true });
+}
+
+/**
+ * Removes a todo item from the todos list based on the given id.
+ *
+ * @param {UUID} id - The id of the todo item to be deleted.
+ * @return {void} This function does not return anything.
+ */
+function deleteTodo(id: UUID): void {
+  const todos = readTodos();
+
+  if (!todos.length) {
+    return;
+  }
+
+  const todoIndex = todos.findIndex(t => t.id === id);
+  
+  if (todoIndex === -1) {
+    throw new Error('Todo not found');
+  }
+
+  todos.splice(todoIndex, 1);
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos}, null, 2));
 }
 
 /**
@@ -113,12 +141,33 @@ function clearDB() {
   fs.writeFileSync(DB_FILE_PATH, '');
 }
 
+//==============================================================================================
 // [SIMULATUION]
 clearDB();
+
+// Cria as todos
 const todo1 = createTodo('Primeira TODO');
 const todo2 = createTodo('Segunda TODO');
 const todo3 = createTodo('Terceira TODO');
-updateTodo(todo3.id, { content: 'Terceira TODO alterada', done: true });
-updateTodoContentById(todo2.id, 'Segunda TODO alterada');
-markTodoAsDone(todo1.id);
+
+// Atualiza uma todo após um segundo da criação
+setTimeout(() => {
+  updateTodo(todo3.id, { content: 'Terceira TODO alterada', done: true });
+}, 1000);
+
+// Atualiza o conteúdo de uma todo após dois segundos da criação
+setTimeout(() => {
+  updateTodoContentById(todo2.id, 'Segunda TODO alterada');
+}, 2000);
+
+// Marca uma todo como concluída após um três segundos da criação
+setTimeout(() => {
+  markTodoAsDone(todo1.id);
+  
+}, 3000);
+
+// Deleta uma todo após quatro segundos da criação
+setTimeout(() => {
+  deleteTodo(todo1.id);
+}, 4000)
 console.log(readTodos());
