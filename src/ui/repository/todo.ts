d609e867522bcc4d32351687e58348258c35f783
Ricardo Ruiz <ricardo.ruiz@grupoboticario.com.br>
2023-11-22 16:57:13 -0300

    feat: parse todos received from server to a correct type

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -18,6 +18,51 @@ interface TodoRespositoryGetOuput {
   pages: number;
 }
 
+/**
+ * Parses the given data and returns an array of Todo objects.
+ *
+ * @param {unknown} data - The data to be parsed.
+ * @return {Todo[]} An array of Todo objects.
+ */
+const parseTodos = (data: unknown): Todo[] => {
+  if (
+    data !== null &&
+    typeof data === "object" &&
+    "todos" in data &&
+    Array.isArray(data.todos)
+  ) {
+    return data.todos.map((todo: unknown) => {
+      if (todo === null && typeof todo !== "object") {
+        throw new Error("Invalid todo from API");
+      }
+
+      const { id, content, date, done } = todo as {
+        id: string;
+        content: string;
+        date: string;
+        done: string;
+      };
+
+      return {
+        id,
+        content,
+        date: new Date(date),
+        done: String(done).toLowerCase() === "true",
+      };
+    });
+  }
+
+  return [];
+};
+
+/**
+ * Retrieves a paginated list of todos from the specified API.
+ *
+ * @param {TodoRepositoryGetParams} params - The parameters for the GET request.
+ * @param {number} params.page - The page number to retrieve.
+ * @param {number} params.limit - The maximum number of todos per page.
+ * @return {Promise<TodoRespositoryGetOuput>} A promise that resolves to the paginated todos.
+ */
 async function get({
   page,
   limit,
@@ -25,7 +70,7 @@ async function get({
   const response = await fetch(TODOS_URL);
   const data = await response.json();
 
-  const allTodos: Todo[] = data.todos || [];
+  const allTodos = parseTodos(data);
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
   const paginatedTodos = allTodos.slice(startIndex, endIndex);
